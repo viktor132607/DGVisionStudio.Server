4a45cff26dd5760a4b5c8fa5282d7925f0742294
viktor132607 <iliev132607@gmail.com>
2026-05-14 17:20:58 +0300

    Temporarily run EF migrations on startup

--- DGVisionStudio.Api/Program.cs
+++ DGVisionStudio.Api/Program.cs
@@ -167,6 +167,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	await db.Database.MigrateAsync();
+}
+
 app.UseSerilogRequestLogging();
 
 app.UseForwardedHeaders();
