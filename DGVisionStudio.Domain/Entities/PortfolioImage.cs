9183f74c5b3ef641c53d453b20b8713cd44e618c
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-02 21:43:21 +0300

    Add media name field

--- DGVisionStudio.Domain/Entities/PortfolioImage.cs
+++ DGVisionStudio.Domain/Entities/PortfolioImage.cs
@@ -7,6 +7,7 @@ public class PortfolioImage
 	public PortfolioAlbum? PortfolioAlbum { get; set; }
 	public string ImageUrl { get; set; } = string.Empty;
 	public string? ThumbnailUrl { get; set; }
+	public string? Name { get; set; }
 	public string? AltText { get; set; }
 	public string? Caption { get; set; }
 	public int Width { get; set; }
