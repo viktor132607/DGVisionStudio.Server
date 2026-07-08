3c39a9984ae6d43279c28574391d7051abb290e5
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-08 23:21:14 +0300

    Refactor public pricing controller to service

--- DGVisionStudio.Api/Controllers/PricingController.cs
+++ DGVisionStudio.Api/Controllers/PricingController.cs
@@ -1,64 +1,16 @@
-using DGVisionStudio.Infrastructure.Data;
+using DGVisionStudio.Api.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Data;
-using System.Data.Common;
 
 namespace DGVisionStudio.Infrastructure.Controllers;
 
 [ApiController]
 [Route("api/pricing")]
-public class PricingController : ControllerBase
+public class PricingController(IPricingService pricingService) : ControllerBase
 {
-    private readonly AppDbContext _context;
-
-    public PricingController(AppDbContext context)
-    {
-        _context = context;
-    }
-
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        await PricingDataSeeder.EnsureTableAsync(_context);
-
-        using var command = _context.Database.GetDbConnection().CreateCommand();
-        command.CommandText = """
-            SELECT "Id", "Title", "Description", "PricingMode", "PriceText", "DisplayOrder", "IsActive", "CreatedAtUtc"
-            FROM "PricingItems"
-            WHERE "IsActive" = TRUE
-            ORDER BY "DisplayOrder", "Id"
-            """;
-
-        var items = await ReadPricingItemsAsync(_context, command);
-        return Ok(items);
-    }
-
-    internal static async Task<List<PricingItemResponse>> ReadPricingItemsAsync(AppDbContext db, DbCommand command)
-    {
-        if (command.Connection?.State != ConnectionState.Open)
-        {
-            await db.Database.OpenConnectionAsync();
-        }
-
-        var items = new List<PricingItemResponse>();
-        await using var reader = await command.ExecuteReaderAsync();
-
-        while (await reader.ReadAsync())
-        {
-            items.Add(new PricingItemResponse(
-                reader.GetInt32(0),
-                reader.GetString(1),
-                reader.GetString(2),
-                reader.GetString(3),
-                reader.IsDBNull(4) ? null : reader.GetString(4),
-                reader.GetInt32(5),
-                reader.GetBoolean(6),
-                reader.GetDateTime(7)
-            ));
-        }
-
-        return items;
+        return Ok(await pricingService.GetActiveAsync());
     }
 }
 
