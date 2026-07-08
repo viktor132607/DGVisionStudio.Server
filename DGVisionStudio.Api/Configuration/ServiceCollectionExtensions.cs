6979a8d17f6e8d81a86f5dc03cf6d2dd8af792e4
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-08 23:57:51 +0300

    Add application service registration extension

---
+++ DGVisionStudio.Api/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+using DGVisionStudio.Api.Services;
+using DGVisionStudio.Application.Interfaces;
+using DGVisionStudio.Infrastructure.Services;
+using DGVisionStudio.Infrastructure.Services.ClientGalleries;
+
+namespace DGVisionStudio.Api.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDGVisionApplicationServices(
+        this IServiceCollection services,
+        StorageOptions storageOptions)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<IPricingService, PricingService>();
+
+        if (storageOptions.UseCloudinary)
+        {
+            services.AddScoped<IFileStorageService, CloudinaryFileStorageService>();
+        }
+        else
+        {
+            services.AddScoped<IFileStorageService, FileStorageService>();
+        }
+
+        services.AddScoped<IAuditLogService, AuditLogService>();
+
+        services.AddScoped<IClientGalleryService, ClientGalleryService>();
+        services.AddScoped<IClientGalleryAdminService, ClientGalleryAdminService>();
+        services.AddScoped<IClientGalleryUserService, ClientGalleryUserService>();
+        services.AddScoped<IClientGalleryAccessService, ClientGalleryAccessService>();
+        services.AddScoped<IClientGalleryPhotoService, ClientGalleryPhotoService>();
+        services.AddScoped<IClientGalleryExpiryService, ClientGalleryExpiryService>();
+
+        services.AddScoped<ClientGalleryMapper>();
+        services.AddScoped<ClientGalleryUploadValidator>();
+        services.AddScoped<ClientGalleryNamingService>();
+
+        services.AddHostedService<ExpiredGalleryCleanupService>();
+        services.AddHostedService<CalendarReminderEmailService>();
+
+        return services;
+    }
+}
