b09e230273ccdcac8aa3431c3d964719f192bb5e
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-09 10:26:23 +0300

    Add centralized API error codes

---
+++ DGVisionStudio.Api/Models/ApiErrorCodes.cs
@@ -0,0 +1,11 @@
+namespace DGVisionStudio.Api.Models;
+
+public static class ApiErrorCodes
+{
+    public const string ValidationError = "ValidationError";
+    public const string Unauthorized = "Unauthorized";
+    public const string Forbidden = "Forbidden";
+    public const string NotFound = "NotFound";
+    public const string Conflict = "Conflict";
+    public const string UnexpectedError = "UnexpectedError";
+}
