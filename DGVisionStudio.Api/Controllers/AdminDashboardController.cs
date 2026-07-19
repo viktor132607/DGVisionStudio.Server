7decd8e2ea4645fc5d73202ce4554d7527a811d4
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-07-19 18:51:05 +0300

    Thin admin dashboard controller

--- DGVisionStudio.Api/Controllers/AdminDashboardController.cs
+++ DGVisionStudio.Api/Controllers/AdminDashboardController.cs
@@ -1,9 +1,12 @@
+using DGVisionStudio.Api.Extensions;
+using DGVisionStudio.Api.Services;
+using DGVisionStudio.Api.Services.Interfaces;
 using DGVisionStudio.Domain.Entities;
 using DGVisionStudio.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DGVisionStudio.Infrastructure.Controllers;
 
@@ -12,68 +15,22 @@ namespace DGVisionStudio.Infrastructure.Controllers;
 [Route("api/admin/dashboard")]
 public class AdminDashboardController : ControllerBase
 {
-	private readonly AppDbContext _context;
-	private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IAdminStatisticsService _service;
 
-	public AdminDashboardController(AppDbContext context, UserManager<ApplicationUser> userManager)
-	{
-		_context = context;
-		_userManager = userManager;
-	}
+    [ActivatorUtilitiesConstructor]
+    public AdminDashboardController(IAdminStatisticsService service)
+    {
+        _service = service;
+    }
 
-	[HttpGet]
-	public async Task<IActionResult> GetStats()
-	{
-		var users = await _userManager.Users.CountAsync();
-		var newUsers = await _userManager.Users.CountAsync(x => !x.IsSeenByAdmin && !x.IsBlocked);
+    public AdminDashboardController(
+        AppDbContext context,
+        UserManager<ApplicationUser> userManager)
+        : this(new AdminStatisticsService(context, userManager))
+    {
+    }
 
-		var contacts = await _context.ContactRequests.CountAsync();
-		var newContactRequests = await _context.ContactRequests
-			.CountAsync(x => !x.IsSeenByAdmin && !x.IsArchived);
-
-		var services = await _context.Services.CountAsync();
-		var testimonials = await _context.Testimonials.CountAsync();
-
-		var portfolioCategories = await _context.PortfolioCategories.CountAsync();
-
-		var portfolioAlbums = await _context.PortfolioAlbums
-			.CountAsync(x => !x.IsUserUploaded);
-
-		var portfolioImages = await _context.PortfolioImages
-			.CountAsync(x => x.PortfolioAlbum != null && !x.PortfolioAlbum.IsUserUploaded);
-
-		var directPrintRequests = await _context.PrintRequests.CountAsync();
-
-		var userUploadedAlbums = await _context.PortfolioAlbums
-			.CountAsync(x => x.IsUserUploaded);
-
-		var printRequests = directPrintRequests + userUploadedAlbums;
-
-		var newDirectPrintRequests = await _context.PrintRequests
-			.CountAsync(x => !x.IsSeenByAdmin);
-
-		var newUserUploadedAlbums = await _context.PortfolioAlbums
-			.CountAsync(x => x.IsUserUploaded && !x.IsSeenByAdmin);
-
-		var newPrintRequests = newDirectPrintRequests + newUserUploadedAlbums;
-
-		return Ok(new
-		{
-			users,
-			newUsers,
-
-			contacts,
-			newContactRequests,
-
-			services,
-			testimonials,
-
-			printRequests,
-			newPrintRequests,
-
-			portfolioCategories,
-			portfolioAlbums,
-			portfolioImages
-		});
-	}
+    [HttpGet]
+    public async Task<IActionResult> GetStats() =>
+        this.ToActionResult(await _service.GetDashboardStatsAsync());
 }
